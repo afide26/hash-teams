d15a7bb01b720fa4ab4bc76561a1c3bfc9124b06
Alan Fidelino <afidedev26@gmail.com>
2018-02-17 20:02:19 +1100

    Finish Player component detail rendering and partial Team component

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
-
-
+import Sidebar from './Sidebar';
+import { getTeamNames } from '../api';
+import parse from 'query-string';
 
 export default class Teams extends Component{
+  state = {
+    loading:false,
+    teamNames:[]
+  }
+
+  componentDidMount(){
+    getTeamNames()
+      .then((teamNames)=>{
+        this.setState(()=>(
+          {
+            loading:false,
+            teamNames
+          }
+        ))
+      })
+  }
+
+
   render(){
+
+    const { loading, teamNames } = this.state;
+    const { match, location } = this.props;
+
     return(
       <div className="container two-column">
-        Teams
+        <Sidebar
+          loading={loading}
+          title="Teams"
+          list={teamNames}
+          {...this.props}
+        />
+        {loading === false && location.pathname ==='/teams'
+          ? <div className="sidebar-instruction">Select a Team</div>
+          : null
+        }
       </div>
     )
   }
