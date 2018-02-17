7550fb897113fd3276a6ea05de5d6b1e5ea59a13
Alan Fidelino <afidedev26@gmail.com>
2018-02-17 11:43:47 +1100

    Set-up the teams

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from './Home';
+import Teams from './Teams';
+import Players from './Players';
+import Navbar from './Navbar';
+import NotFound from './NotFound';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/teams" component={Teams}/>
+            <Route path="/players" component={Players}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+
+export default function NotFound(){
+  return(
+    <div className="container">
+      <h1 className="text-center">404</h1>
+      <p className="text-center">The page you are looking for was not found in this application</p>
+    </div>
+  )
+}
